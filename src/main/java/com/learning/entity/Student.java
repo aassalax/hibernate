d3cfb78f0152b5261398fce6c22305bf35e55980
aassalax <aassali.dev@gmail.com>
2025-04-18 22:26:21 +0200

    id annotation

--- src/main/java/com/learning/entity/Student.java
+++ src/main/java/com/learning/entity/Student.java
@@ -1,7 +1,34 @@
 package com.learning.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) //JPA choisit la stratégie appropriée selon la base de données utilisée.
+    //@GeneratedValue(strategy = GenerationType.TABLE) //Utilise une table spéciale pour générer les identifiants uniques. C'est portable mais moins performant.
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE) //Utilise une séquence de la base de données (comme CREATE SEQUENCE) pour générer les identifiants. Performant si la base la supporte (ex: PostgreSQL, Oracle).
+    //@GeneratedValue(strategy = GenerationType.IDENTITY) //Laisse la base gérer l’auto-incrémentation via une colonne IDENTITY. Typiquement pour MySQL, SQL Server. Génère l'id au moment de l'insertion.
+    private Long id;
+
+    private String name;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
