d38f5bfca0e6d42c443887ce43678d9b31a35d47
aassalax <aassali.dev@gmail.com>
2025-04-22 12:38:31 +0200

    create associate repository extends JPARepository

---
+++ src/main/java/com/learning/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.learning.repository;
+
+import com.learning.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUserName(String userName);
+}
